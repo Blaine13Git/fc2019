e9aae7a25b6e56e016d350cedb00a052376a867b
fengchang <fengchang@aicaigroup.com>
2018-12-21 18:47:05 +0800

    初始化项目

---
+++ src/main/java/windsing13/fc2019/Fc2019Application.java
@@ -0,0 +1,14 @@
+package windsing13.fc2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fc2019Application.class, args);
+    }
+
+}
+
